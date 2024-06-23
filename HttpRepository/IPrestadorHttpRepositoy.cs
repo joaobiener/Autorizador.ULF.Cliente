ef4136fb20f254d5f2a31bb5de3a474b0b5c8509
João Carlos Barreto Biener Pedro <joaobiener@hotmail.com>
2024-06-23 09:56:43 -0300

    Inclusao de repositorys

---
+++ HttpRepository/IPrestadorHttpRepositoy.cs
@@ -0,0 +1,13 @@
+﻿using Autorizador.ULF.Cliente.Features;
+using Autorizador.ULF.Services.Shared.DataTransferObjects;
+using Autorizador.ULF.Services.Shared.RequestFeatures;
+
+
+namespace Autorizador.ULF.Cliente.HttpRepository
+{
+    public interface IPrestadorHttpRepositoy
+	{
+		 Task<PagingResponse<PrestadorDto>> GetPrestadores(PrestadorParameters parameters);
+
+	}
+}
---
+++ HttpRepository/IProcedimentoHttpRepository.cs
@@ -0,0 +1,17 @@
+﻿using Autorizador.ULF.Cliente.Features;
+using Entities.Models;
+using Autorizador.ULF.Services.Shared.DataTransferObjects;
+using Shared.RequestFeatures;
+using Autorizador.ULF.Services.Shared.RequestFeatures;
+using Entities.Models.Infomed;
+
+
+namespace Autorizador.ULF.Cliente.HttpRepository
+{
+	public interface IProcedimentoHttpRepository
+	{
+		Task<PagingResponse<Procedimento>> GetAllProcedimentosAsync(
+           PrestadorParameters parameters);
+		//Task<IEnumerable<InfServicoDto>> GetAllServicosBySearchTerm(string searchTerm);
+	}
+}
